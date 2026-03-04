0aa2556102a0aa0ceef5e028b53e23a2a48eb989
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2026-03-04 06:56:03 +0000

    Add a web app manifest
    François Beaufort
    François Beaufort
    Pete LePa

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,6 @@
-const CACHE_NAME = 'ongon-bd-cache-v2';
-const urlsToCache = [
+// ONGON BD PWA Service Worker
+const CACHE_NAME = 'ongon-cache-v1';
+const ASSETS_TO_CACHE = [
   '/',
   '/manifest.json',
   '/favicon.ico'
@@ -7,35 +8,26 @@ const urlsToCache = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
