ea564e6779e613570eb018b4097d1b4269d7becf
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2026-03-04 07:25:04 +0000

    Try fixing this error: `Runtime Error: Module [project]/node_modules/luc

--- public/sw.js
+++ public/sw.js
@@ -1,41 +1,23 @@
 
-const CACHE_NAME = 'ongon-cache-v3';
-const urlsToCache = [
+const CACHE_NAME = 'ongon-bd-v1';
+const ASSETS = [
   '/',
   '/manifest.json',
-  '/favicon.ico',
+  '/favicon.ico'
 ];
 
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
