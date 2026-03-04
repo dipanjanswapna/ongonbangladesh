6588ad9616c1d8aed69f32c7e0dbbf33af6e984d
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2026-03-04 06:29:41 +0000

    ওয়েব অ্যাপকে মোবাইল/ডেস্কটপে ডাউনলোডযোগ্য করার উপায় ১. Progressive Web A

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 
-const CACHE_NAME = 'ongon-cache-v1';
-const urlsToCache = [
+const CACHE_NAME = 'ongon-bd-v1';
+const ASSETS_TO_CACHE = [
   '/',
   '/manifest.json',
   'https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;500;600;700&family=Alegreya:wght@400;700&display=swap'
@@ -8,19 +8,16 @@ const urlsToCache = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
